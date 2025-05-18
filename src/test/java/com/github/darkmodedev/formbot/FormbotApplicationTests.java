5c24bd932473bd8ec08f4fd51bf7b753c1d58224
KauaCorreaArruda <kaua.correa.arruda@gmail.com>
2025-05-18 15:58:31 -0300

    commit inicial formbot

---
+++ src/test/java/com/github/darkmodedev/formbot/FormbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.darkmodedev.formbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
